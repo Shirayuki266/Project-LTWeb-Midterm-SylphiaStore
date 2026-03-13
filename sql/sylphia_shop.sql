e56f46a25ab394b9f696a789b5652a4af482e933
hoanglecodingmachine <thanhtanicluod@gmail.com>
2026-03-13 14:49:09 +0700

    13/03/26 test setup_dp lan 1

--- sql/sylphia_shop.sql
+++ sql/sylphia_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 12, 2026 lúc 09:13 AM
+-- Thời gian đã tạo: Th3 13, 2026 lúc 08:44 AM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `danh_muc_ban_`
+-- Cơ sở dữ liệu: `sylphia_shop`
 --
 
 -- --------------------------------------------------------
